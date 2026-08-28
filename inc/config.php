afb5e048b2ced30149413d5db5f6769fee6f2820
Comtonet <info@comtonet.fr>
2026-08-28 19:53:31 +0200

    feat: centralize site metrics and delivery delays

---
+++ inc/config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    // Chiffres globaux affichés sur le site.
+    'projects_count' => 89000,
+    'google_rating' => 4.6,
+    'google_reviews' => 319,
+
+    // Délais : modifier ici pour répercuter partout où ces helpers sont utilisés.
+    'delay_standard_days' => 1,
+    'delay_eco_days' => 2,
+    'delay_collective_min_days' => 5,
+    'delay_collective_max_days' => 10,
+    'delay_quote_hours' => 24,
+];
