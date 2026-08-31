d7d3589f2729bc3da3090606029f34ec90720c38
Comtonet <info@comtonet.fr>
2026-08-31 14:54:27 +0200

    Load fidelity patch for Denis calculator UI

--- pages/devis-en-ligne.php
+++ pages/devis-en-ligne.php
@@ -60,3 +60,4 @@ $quoteConfig = [
 
 <script>window.QUOTE_CONFIG = <?= json_encode($quoteConfig, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) ?>;</script>
 <script src="/assets/js/devis-calculateur.js?v=2" defer></script>
+<script src="/assets/js/devis-calculateur-fidelite.js?v=1" defer></script>
