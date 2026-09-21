c9c83abd1e3f64b0c4923e04db66217ca6c22ae0
Comtonet <info@comtonet.fr>
2026-09-21 15:17:55 +0200

    Refresh quote script after contact form fix

--- pages/devis-en-ligne.php
+++ pages/devis-en-ligne.php
@@ -157,4 +157,4 @@ $quoteConfig = [
 </section>
 <script>window.QUOTE_CONFIG = <?= json_encode($quoteConfig, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) ?>;</script>
 <script src="/assets/js/devis-calculateur-lite.php?v=11" defer></script>
-<script src="/assets/js/devis-site-lite.js?v=10" defer></script>
+<script src="/assets/js/devis-site-lite.js?v=11" defer></script>
