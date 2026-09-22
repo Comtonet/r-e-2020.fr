c6852bebca02ee311f273b3c72572b85d5f460cb
Comtonet <info@comtonet.fr>
2026-09-22 11:28:11 +0200

    Actualise le cache du retour devis compte existant

--- pages/devis-en-ligne.php
+++ pages/devis-en-ligne.php
@@ -162,5 +162,5 @@ $quoteConfig = [
   <div class="infotip noprint" id="quoteInfotip" role="tooltip"><div class="infotip-body"></div></div>
 </section>
 <script>window.QUOTE_CONFIG = <?= json_encode($quoteConfig, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) ?>;</script>
-<script src="/assets/js/devis-calculateur-lite.php?v=20" defer></script>
-<script src="/assets/js/devis-site-lite.js?v=20" defer></script>
+<script src="/assets/js/devis-calculateur-lite.php?v=21" defer></script>
+<script src="/assets/js/devis-site-lite.js?v=21" defer></script>
